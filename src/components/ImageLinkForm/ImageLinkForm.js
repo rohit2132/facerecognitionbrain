2e885cc454ba01ab3f4b613248ad9140cb2bca23
Rohit K <k6346246@gmail.com>
2021-07-16 11:02:46 +0530

    some changes in ImageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             <div className='center'>
                 <div className='form pa4 br3 shadow-5 center'>
                 <input className='f4 pa2 w-70 center' type='text' onChange= {onInputChange}/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick= {onButtonSubmit} >Detect</button>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick= {onButtonSubmit} >Face Detect</button>
                 </div>
             </div>
         </div>
